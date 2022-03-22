a41a9f5eb30f8aa009bcf6782c73dd18556f9bae
Dominik Fabisiewicz <domi.fab98@gmail.com>
2022-03-22 21:45:57 +0100

    go through slashcommand example;

--- index.js
+++ index.js
@@ -10,5 +10,18 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// Login to Discord with your client's token
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name ${interaction.guild.name}. \n${interaction.guild.memberCount} Members.`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 client.login(token);
